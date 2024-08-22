47c2c3600bb08724546b5c1c28ad854f967882c6
Gitrathirad <37181342+Gitrathirad@users.noreply.github.com>
2024-08-22 13:29:42 +0530

    Update Answer.tsx

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -68,21 +68,7 @@ export const Answer = ({ answer, onCitationClicked, onExectResultClicked }: Prop
   }, [appStateContext?.state.feedbackState, feedbackState, answer.message_id])
 
   const createCitationFilepath = (citation: Citation, index: number, truncate: boolean = false) => {
-    let citationFilename = ''
-
-    if (citation.filepath) {
-      const part_i = citation.part_index ?? (citation.chunk_id ? parseInt(citation.chunk_id) + 1 : '')
-      if (truncate && citation.filepath.length > filePathTruncationLimit) {
-        const citationLength = citation.filepath.length
-        citationFilename = `${citation.filepath.substring(0, 20)}...${citation.filepath.substring(citationLength - 20)} - Part ${part_i}`
-      } else {
-        citationFilename = `${citation.filepath} - Part ${part_i}`
-      }
-    } else if (citation.filepath && citation.reindex_id) {
-      citationFilename = `${citation.filepath} - Part ${citation.reindex_id}`
-    } else {
-      citationFilename = `Citation ${index}`
-    }
+   
     return ''
   }
 
